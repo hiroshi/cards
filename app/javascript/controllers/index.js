5cbb2d077471f9c016dca8e880d7f3f2adc1cabf
hiroshi <hiroshi3110@gmail.com>
2022-02-12 19:06:02 +0900

    Initialize the StimulusReflex Client

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,6 +1,10 @@
 // Import and register all your controllers from the importmap under controllers/*
 
+// https://blog.minthesize.com/stimulus-reflex-importmaps
+import StimulusReflex from "stimulus_reflex"
 import { application } from "controllers/application"
+// https://blog.minthesize.com/stimulus-reflex-importmaps
+import { cable } from "@hotwired/turbo-rails"
 
 // Eager load all controllers defined in the import map under controllers/**/*_controller
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
@@ -9,3 +13,7 @@ eagerLoadControllersFrom("controllers", application)
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
+
+// https://blog.minthesize.com/stimulus-reflex-importmaps
+const consumer = await cable.getConsumer()
+StimulusReflex.initialize(application, { consumer, debug: true })
